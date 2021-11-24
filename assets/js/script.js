547da196a9e84f1e4c4ac4549d7ce49f9a6393cb
edublkc <89402489+edublkc@users.noreply.github.com>
2021-11-24 16:22:05 -0300

    First part

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+const menuMobileIcon = document.querySelector('.menu-mobile--icon')
+const menuMobile = document.querySelector('#menu-items')
+const menu = document.querySelector('#navbar')
+const goToTop = document.querySelector('.go-to-top')
+const menuOptions = document.querySelectorAll('#menu-items a')
+
+
+menuMobileIcon.addEventListener('click',function(){
+    menuMobile.classList.toggle('active')
+})
+
+
+window.addEventListener('scroll',function(){
+
+    if(window.pageYOffset > menu.offsetTop){
+        menu.classList.add('sticky')
+        goToTop.classList.add('active')
+    }else{
+        menu.classList.remove('sticky')
+        goToTop.classList.remove('active')
+    }
+})
+
+goToTop.addEventListener('click',function(){
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior: "smooth"
+    });
+})
+
+
+menuOptions.forEach((link) =>{
+    link.addEventListener('click',function(){
+        removeAllActiveMenuClass()
+        if(!link.classList.contains('active')){
+            link.classList.add('active')
+        }
+    })
+})
+
+function removeAllActiveMenuClass(){
+    menuOptions.forEach((link) =>{
+        link.classList.remove('active')
+    })
+}
